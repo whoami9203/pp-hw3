acd9b56d75191b3bead6b6aa112f8958bc060f42
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2024-11-08 14:54:03 +0800

    first try shared memory

--- hw3.cu
+++ hw3.cu
@@ -105,8 +105,10 @@ void write_png(const char* filename, png_bytep image, const unsigned height, con
 // cudaMemPrefetchAsync(a, size, deviceId);
 // cudaMemPrefetchAsync(c, size, cudaCpuDeviceId);
 
-__global__ void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels) {
-    int x, y, v, u;
+__global__ void sobel(unsigned char* s, unsigned char* t, unsigned height, unsigned width, unsigned channels, int TILE_WIDTH) {
+    extern __shared__ unsigned char sharedMem[][TILE_WIDTH * channels];
+
+    int vv, uu, v, u;
     int R, G, B;
     int val[MASK_N * 3] = {0};
     int adjustX = 1;
@@ -114,56 +116,112 @@ __global__ void sobel(unsigned char* s, unsigned char* t, unsigned height, unsig
     int xBound = 2;
     int yBound = 2;
     int total_pixel = width * height;
+    int cur_mem_row = 0;
+    int end_mem_row = 4;
 
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
-    int stride = blockDim.x * gridDim.x;    
+    int rows_per_block = height / 1280 + 1;
+    int numOfChunk = (width+5) / 6400 + 1;
+    int chunk_index = blockIdx.x % numOfChunk;
+    int start_column = chunk_index * TILE_WIDTH;
+    int end_column = start_column + TILE_WIDTH > (width+5) ? width+5 : start_column + TILE_WIDTH;
 
-    while(index < total_pixel){
-        y = index / width;
-        x = index % width;
+    int start_row = blockIdx.x / numOfChunk * rows_per_block;
+    int end_row = start_row + rows_per_block;
+    if(end_row > height)
+        end_row = height;
 
-        val[0] = 0;
-        val[1] = 0;
-        val[2] = 0;
-        val[3] = 0;
-        val[4] = 0;
-        val[5] = 0;
-        
-        for (v = -yBound; v < yBound + adjustY; ++v) {
-            for (u = -xBound; u < xBound + adjustX; ++u) {
-                R = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 2];
-                G = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 1];
-                B = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 0];
-
-                val[2] += R * mask[0][u + xBound][v + yBound];
-                val[1] += G * mask[0][u + xBound][v + yBound];
-                val[0] += B * mask[0][u + xBound][v + yBound];
-
-                val[5] += R * mask[1][u + xBound][v + yBound];
-                val[4] += G * mask[1][u + xBound][v + yBound];
-                val[3] += B * mask[1][u + xBound][v + yBound];
+    if (start_row < end_row){
+        int len = end_column - start_column + 1;
+        int stride = blockDim.x;
+        for(int i=0; i<len; i+=stride){
+            for(int j=0; j<5; ++j){
+                sharedMem[j][channels * i + 0] = s[channels * ((start_row+j)*(width+5) + chunk_index*TILE_WIDTH + i) + 0];
+                sharedMem[j][channels * i + 1] = s[channels * ((start_row+j)*(width+5) + chunk_index*TILE_WIDTH + i) + 1];
+                sharedMem[j][channels * i + 2] = s[channels * ((start_row+j)*(width+5) + chunk_index*TILE_WIDTH + i) + 2];
             }
         }
+    }
 
-        float totalR = 0.0;
-        float totalG = 0.0;
-        float totalB = 0.0;
-        
-        totalR += val[2] * val[2] + val[5] * val[5];
-        totalG += val[1] * val[1] + val[4] * val[4];
-        totalB += val[0] * val[0] + val[3] * val[3];
+    __syncthreads();
 
-        totalR = sqrt(totalR) / SCALE;
-        totalG = sqrt(totalG) / SCALE;
-        totalB = sqrt(totalB) / SCALE;
-        const unsigned char cR = (totalR > 255.0) ? 255 : totalR;
-        const unsigned char cG = (totalG > 255.0) ? 255 : totalG;
-        const unsigned char cB = (totalB > 255.0) ? 255 : totalB;
-        t[channels * (width * y + x) + 2] = cR;
-        t[channels * (width * y + x) + 1] = cG;
-        t[channels * (width * y + x) + 0] = cB;
+    int len = end_column - start_column + 1;
+    int stride = blockDim.x;    
 
-        index += stride;
+    while(start_row < end_row){
+        for(int index=0; index<len; index+=stride){
+            val[0] = 0;
+            val[1] = 0;
+            val[2] = 0;
+            val[3] = 0;
+            val[4] = 0;
+            val[5] = 0;
+            
+            for (v = -yBound; v < yBound + adjustY; ++v) {
+                for (u = -xBound; u < xBound + adjustX; ++u) {
+                    R = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 2];
+                    G = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 1];
+                    B = s[channels * ((width+5) * (y+2 + v) + (x+2 + u)) + 0];
+
+                    val[2] += R * mask[0][u + xBound][v + yBound];
+                    val[1] += G * mask[0][u + xBound][v + yBound];
+                    val[0] += B * mask[0][u + xBound][v + yBound];
+
+                    val[5] += R * mask[1][u + xBound][v + yBound];
+                    val[4] += G * mask[1][u + xBound][v + yBound];
+                    val[3] += B * mask[1][u + xBound][v + yBound];
+                }
+            }
+
+            for (v = 0; v < 5; ++v) {
+                for (u = 0; u < 5; ++u) {
+                    vv = (v + cur_mem_row) % 5;
+                    uu = (u + cur_mem_row) % 5;
+
+                    R = sharedMem[channels * ((TILE_WIDTH) * (vv) + (uu)) + 2];
+                    G = sharedMem[channels * ((TILE_WIDTH) * (vv) + (uu)) + 1];
+                    B = sharedMem[channels * ((TILE_WIDTH) * (vv) + (uu)) + 0];
+
+                    val[2] += R * mask[0][u][v];
+                    val[1] += G * mask[0][u][v];
+                    val[0] += B * mask[0][u][v];
+
+                    val[5] += R * mask[1][u][v];
+                    val[4] += G * mask[1][u][v];
+                    val[3] += B * mask[1][u][v];
+                }
+            }
+
+            float totalR = 0.0;
+            float totalG = 0.0;
+            float totalB = 0.0;
+            
+            totalR += val[2] * val[2] + val[5] * val[5];
+            totalG += val[1] * val[1] + val[4] * val[4];
+            totalB += val[0] * val[0] + val[3] * val[3];
+
+            totalR = sqrt(totalR) / SCALE;
+            totalG = sqrt(totalG) / SCALE;
+            totalB = sqrt(totalB) / SCALE;
+            const unsigned char cR = (totalR > 255.0) ? 255 : totalR;
+            const unsigned char cG = (totalG > 255.0) ? 255 : totalG;
+            const unsigned char cB = (totalB > 255.0) ? 255 : totalB;
+            t[channels * (width * start_row + chunk_index * TILE_WIDTH + index) + 2] = cR;
+            t[channels * (width * start_row + chunk_index * TILE_WIDTH + index) + 1] = cG;
+            t[channels * (width * start_row + chunk_index * TILE_WIDTH + index) + 0] = cB;
+
+            start_row++;
+            if(start_row < end_row){
+                cur_mem_row = (cur_mem_row + 1) % 5;
+                end_mem_row = (end_mem_row + 1) % 5;
+                for(int i=0; i<len; i+=stride){
+                    sharedMem[end_mem_row][channels * i + 0] = s[channels * ((start_row+2)*(width+5) + chunk_index*TILE_WIDTH + i) + 0];
+                    sharedMem[end_mem_row][channels * i + 1] = s[channels * ((start_row+2)*(width+5) + chunk_index*TILE_WIDTH + i) + 1];
+                    sharedMem[end_mem_row][channels * i + 2] = s[channels * ((start_row+2)*(width+5) + chunk_index*TILE_WIDTH + i) + 2];
+                }
+
+                __syncthreads();
+            }
+        }
     }
 }
 
@@ -225,7 +283,12 @@ int main(int argc, char** argv) {
     std::cout << "Image Copy Time: " << elapsed_seconds.count() * 1000.0 << " ms" << std::endl;
 
     auto start_sobel = std::chrono::high_resolution_clock::now();
-    sobel<<<numberOfBlocks, threadsPerBlock>>>(mod_src_img, dst_img, height, width, channels);
+    
+    int rows_per_block = height / 1280 + 1;
+    int numOfColumn = (width + 5) / 6400 + 1;
+    int columnWidth = (width + 5 + numOfColumn - 1) / numOfColumn;
+    int sharedMemSize = 5 * columnWidth * channels * sizeof(unsigned char);
+    sobel<<<numberOfBlocks, threadsPerBlock, sharedMemSize>>>(mod_src_img, dst_img, height, width, channels, columnWidth);
     cudaDeviceSynchronize();
 
     auto end_sobel = std::chrono::high_resolution_clock::now();
@@ -234,7 +297,13 @@ int main(int argc, char** argv) {
 
     cudaMemPrefetchAsync(dst_img, height * width * channels * sizeof(unsigned char), cudaCpuDeviceId);
 
+    auto start_write = std::chrono::high_resolution_clock::now();
+
     write_png(argv[2], dst_img, height, width, channels);
+
+    auto end_write = std::chrono::high_resolution_clock::now();
+    elapsed_seconds = end_write - start_write;
+    std::cout << "Write Time: " << elapsed_seconds.count() * 1000.0 << " ms" << std::endl;
 
     // free(src_img);
     // fprintf(stderr, "free src_img\n");
@@ -242,7 +311,7 @@ int main(int argc, char** argv) {
     cudaFree(mod_src_img);
 
     auto end_all = std::chrono::high_resolution_clock::now();
-    elapsed_seconds = end_sobel - start_sobel;
+    elapsed_seconds = end_all - start_all;
     std::cout << "Total Time: " << elapsed_seconds.count() * 1000.0 << " ms" << std::endl;
 
     return 0;
